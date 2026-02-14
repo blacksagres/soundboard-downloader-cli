5137029427c64c64300c42d9320019fc6c3aabee
Geraldo dos Santos <geraldo.nogueirabs@gmail.com>
2026-02-14 21:43:52 +0100

    fix: my big brain sorting that broke the links

--- src/service/my-instants.service.ts
+++ src/service/my-instants.service.ts
@@ -58,12 +58,10 @@ export const getSoundNodes = async (searchString: string) => {
 
   const allLinksResolved = await Promise.all(allDownloadLinks);
 
-  const finalList = allLabels
-    .toSorted((a, b) => a.localeCompare(b))
-    .map((label, index) => ({
-      label,
-      download_url: allLinksResolved[index] ?? "not-found",
-    }));
+  const finalList = allLabels.map((label, index) => ({
+    label,
+    download_url: allLinksResolved[index] ?? "not-found",
+  }));
 
-  return finalList;
+  return finalList.toSorted((a, b) => a.label.localeCompare(b.label));
 };
