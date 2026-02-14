15ca588fe4ae5fc4157da9b6421723ffcadee1e1
Geraldo dos Santos <geraldo.nogueirabs@gmail.com>
2026-02-14 20:00:41 +0100

    feature(ui): add a better commander

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,9 @@
-import { initLip, Lipgloss, huh } from "charsm";
 import { getSoundNodes } from "./service/my-instants.service";
 import ora from "ora";
+import { input, select } from "@inquirer/prompts";
 
 (async function () {
-  const ini = await initLip();
-  const lip = new Lipgloss();
-
-  huh.SetTheme("dracula");
-
-  const m = huh.Confirm("Do you like pizza?", "Yes", "No");
-  console.log(m.run());
-
-  const i = new huh.NewInput(
-    {
-      Title: "What sounds are you looking for?",
-      Description: "Search for a specific sound effect.",
-      Placeholder: "e.g., memes, resident evil",
-      validators: "no_numbers,required",
-    },
-    0, // Mode: Single Input
-  );
-  i.load();
-  const searchTerm = i.run();
+  const answer = await input({ message: "Enter your name" });
 
   const spinner = ora({
     text: "Loading result...",
@@ -29,23 +11,17 @@ import ora from "ora";
     spinner: "bouncingBall",
   }).start();
 
-  const sounds = await getSoundNodes(searchTerm);
-
-  const tabledata = {
-    headers: ["Sound name", "Download URL"],
-    rows: sounds.map((sound) => [sound.label, sound.download_url]),
-  };
+  const sounds = await getSoundNodes(answer);
 
   spinner.stop();
 
-  const table = lip.newTable({
-    data: tabledata,
-    table: { border: "rounded", color: "99" },
-    header: { color: "212", bold: true },
-    rows: {
-      even: { color: "246" },
-    },
+  const selection = await select({
+    message: "Which one to download?",
+    choices: sounds.map((sound) => ({
+      name: sound.label,
+      value: sound.download_url,
+    })),
   });
 
-  console.log(table);
+  console.table(selection);
 })();
