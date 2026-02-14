c314d5b4f6c5561817fb763bf15d782d4dc4f67d
Geraldo dos Santos <geraldo.nogueirabs@gmail.com>
2026-02-14 20:30:51 +0100

    chore: complete the commands to make it all work

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { getSoundNodes } from "./service/my-instants.service";
 import ora from "ora";
 import { input, select } from "@inquirer/prompts";
+import open from "open";
+import { downloadFile } from "./service/file-downloader.service";
 
 (async function () {
-  const answer = await input({ message: "Enter your name" });
+  const answer = await input({
+    message: "What sound effects are you looking for?",
+  });
 
   const spinner = ora({
     text: "Loading result...",
@@ -19,9 +23,49 @@ import { input, select } from "@inquirer/prompts";
     message: "Which one to download?",
     choices: sounds.map((sound) => ({
       name: sound.label,
-      value: sound.download_url,
+      value: `${sound.label}||${sound.download_url}` as const,
     })),
   });
 
-  console.table(selection);
+  const [label, downloadUrl] = selection.split("||");
+
+  if (!downloadUrl) {
+    throw new ReferenceError(`There is no download URL for: ${label}`);
+  }
+
+  const actions = [
+    {
+      name: "Download",
+      value: "action:download",
+    },
+    {
+      name: "Play",
+      value: "action:play",
+    },
+  ] as const;
+
+  const action = await select({
+    message: `Selected: ${label}`,
+    choices: actions,
+  });
+
+  if (action === "action:play") {
+    open(downloadUrl);
+  }
+
+  if (action === "action:download") {
+    const downloadSpinner = ora({
+      text: "Downloading file...",
+      color: "cyan",
+      spinner: "growHorizontal",
+    }).start();
+
+    const downloadFileName = downloadUrl.split("/").pop();
+
+    downloadFile(downloadUrl, {
+      destination: `./temp/${downloadFileName}`,
+      onStart: () => downloadSpinner.start(),
+      onFinish: () => downloadSpinner.stop(),
+    });
+  }
 })();
