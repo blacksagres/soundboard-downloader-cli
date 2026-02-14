b3d7e2026eb9cedee3ad44ef47ab081d0c1f3510
Geraldo dos Santos <geraldo.nogueirabs@gmail.com>
2026-02-14 18:19:26 +0100

    feature: display all results in terminal

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,42 @@
-const table = Array.from(Array(10)).map((m, index) => ({ m, index }));
+import { initLip, Lipgloss, huh } from "charsm";
+import { getSoundNodes } from "./service/my-instants.service";
 
-console.table(table);
+(async function () {
+  const ini = await initLip();
+  const lip = new Lipgloss();
+
+  huh.SetTheme("dracula");
+
+  const m = huh.Confirm("Do you like pizza?", "Yes", "No");
+  console.log(m.run());
+
+  const i = new huh.NewInput(
+    {
+      Title: "What sounds are you looking for?",
+      Description: "Search for a specific sound effect.",
+      Placeholder: "e.g., memes, resident evil",
+      validators: "no_numbers,required",
+    },
+    0, // Mode: Single Input
+  );
+  i.load();
+  const searchTerm = i.run();
+
+  const sounds = await getSoundNodes(searchTerm);
+
+  const tabledata = {
+    headers: ["Sound name", "Download URL"],
+    rows: sounds.map((sound) => [sound.label, sound.download_url]),
+  };
+
+  const table = lip.newTable({
+    data: tabledata,
+    table: { border: "rounded", color: "99" },
+    header: { color: "212", bold: true },
+    rows: {
+      even: { color: "246" },
+    },
+  });
+
+  console.log(table);
+})();
