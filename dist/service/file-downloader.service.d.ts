84a14a0c9c3931682965501fd5734d8411203782
Geraldo dos Santos <geraldo.nogueirabs@gmail.com>
2026-02-14 22:00:45 +0100

    chore: remove all references to global install

--- dist/service/file-downloader.service.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const downloadFile: (url: string, config: {
-    destination: string;
-    onStart?: () => void;
-    onFinish?: () => void;
-}) => void;
-//# sourceMappingURL=file-downloader.service.d.ts.map
